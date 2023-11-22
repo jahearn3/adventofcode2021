9aacc20ffebda4699d240d86b89ae216c2c4ce81
jahearn3 <josephahearn3@gmail.com>
2023-11-22 15:30:57 -0800

    day 14 part 2

--- day14.py
+++ day14.py
@@ -24,6 +24,7 @@ for i in range(10):
                 continue
     next_template += template[-1]
     template = next_template
+    # print(i + 1, len(template))
 
 # Find most common and least common elements
 frequencies = Counter(template).most_common()
@@ -31,3 +32,27 @@ most_common = frequencies[0]
 least_common = frequencies[-1]
 ans = most_common[1] - least_common[1]
 print(ans) # Part 1 answer: 2068 
+
+# Part 2 with help from Jonathan Paulson
+template = data[0]
+
+pair_counter = Counter()
+for i in range(len(template) - 1):
+    pair_counter[template[i]+template[i+1]] += 1
+
+for i in range(40):
+    C2 = Counter()
+    for k in pair_counter:
+        C2[k[0]+pair_insertion_rules[k]] += pair_counter[k]
+        C2[pair_insertion_rules[k]+k[1]] += pair_counter[k]
+    pair_counter = C2
+
+    if i + 1 in [10, 40]:
+        CF = Counter()
+        for k in pair_counter:
+            CF[k[0]] += pair_counter[k]
+        CF[template[-1]] += 1
+        print(max(CF.values()) - min(CF.values()))
+
+# 5647232997696 too high
+# Part 2 answer: 2158894777814 
